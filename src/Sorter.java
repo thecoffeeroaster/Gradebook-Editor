416dcf8923ed47772a85bd19a93565cdfdcc96ea
Josh <91155952+thecoffeeroaster@users.noreply.github.com>
2022-02-09 19:34:32 -0800

    Added Searcher and Sorter classes.

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,3 +1,17 @@
+/************************************************************************************************
+ * CLASS: Sorter (Sorter.java)
+ *
+ * DESCRIPTION
+ * (ENTER DESCRIPTION)
+ *
+ * COURSE AND PROJECT INFORMATION
+ * CSE 205 Object Oriented Programming and Data Structures, Spring A 2022
+ * Project Number: project-3
+ *
+ * AUTHORS: Joshua McKenzie, ASU ID: 1209056897, jsmckenz@asu.edu
+ *        | Sean Loehr, ASU ID: 1222435526, smloehr@asu.edu
+ ************************************************************************************************/
+
 public class Sorter
 {
 }
