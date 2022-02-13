44a240dafedca576aa7679b1034a56efd2227b03
smloehr <smloehr@asu.edu>
2022-02-12 21:55:47 -0700

    starting sorter

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Collections;
+
 /************************************************************************************************
  * CLASS: Sorter (Sorter.java)
  *
